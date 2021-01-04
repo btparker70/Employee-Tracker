11bf0e4b1d0c3b869bc7c230314a6eb7b6a5bc4c
Brian Parker <btparker70@gmail.com>
2021-01-03 17:47:27 -0800

    added basic layout for inquirer prompts

--- app.js
+++ app.js
@@ -1,1 +1,84 @@
-// inquirer and stuff here
+// Dependencies
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var cTable = require("console.table")
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 8800
+    port: 8800,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "",
+    database: "employees_db"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+});
+
+// Prompts the user for what action they should take
+function start() {
+    inquirer
+        .prompt({
+            name: "optionList",
+            type: "list",
+            message: "Would you like to view or add?",
+            choices: ["POST", "BID", "EXIT"]
+        })
+        .then(function (answer) {
+            // based on their answer, either call the bid or the post functions
+            if (answer.optionList === "VIEW") {
+                viewPrompt();
+            }
+            else if (answer.optionList === "ADD") {
+                addPrompt();
+            } else {
+                connection.end();
+            }
+        });
+}
+
+// Choose to view departments, roles, employees 
+function viewPrompt() {
+    inquirer
+        .prompt({
+            name: "viewList",
+            type: "list",
+            message: "Would you like to view departments, roles or employees?",
+            choices: ["DEPARTMENTS", "ROLES", "EMPLOYEES", "BACK"]
+        })
+        .then(function (answer) {
+            // based on their answer, either call the bid or the post functions
+            if (answer.viewList === "DEPARTMENTS") {
+                viewDepartments();
+            }
+            else if (answer.viewList === "ROLES") {
+                viewRoles();
+            }
+            else if (answer.viewList === "EMPLOYEES") {
+                viewEmployees();
+            }
+            else {
+                start();
+            }
+        });
+}
+
+console.table([
+    {
+      name: 'foo',
+      age: 10
+    }, {
+      name: 'bar',
+      age: 20
+    }
+  ]);
